2de03bab9c36ec546f6d4bdac68121f2894dd0b2
fortmarek <marekfort@me.com>
2025-03-06 12:50:10 +0100

    Step 1

--- App/Modules/TuistAppKit/Project.swift
+++
@@ -1,4 +0,0 @@
-import ProjectDescription
-import ProjectDescriptionHelpers
-
-let project = Project.tuist(module: .kit)
--- App/Package.swift
+++
@@ -1,8 +0,0 @@
-// swift-tools-version: 6.0
-@preconcurrency import PackageDescription
-
-let package = Package(
-    name: "TuistApp",
-    dependencies: [
-        .package(url: "https://github.com/httpswift/swifter", .upToNextMajor(from: "1.5.0"))
-    ])
